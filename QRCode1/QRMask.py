f7ffbf70272eaf7e42a66f359cb8da38ca367f5f
Cornbald <adrien.lacroix@etu.univ-amu.fr>
2022-01-26 11:54:58 +0100

    Changement Mask

--- QRCode1/QRMask.py
+++ QRCode1/QRMask.py
@@ -1,18 +1,14 @@
 import QRFixedPatterns as FP
 
-def Mask1ApplicationX(Y = 0, X = 0):
-
-    while X < FP.V1:
-        if FP.A[Y,X] == 1:
-            FP.A[Y,X] = 0
-            X = X +1
-        else:
-            FP.A[Y, X] = 1
-            X = X + 1
-
-def Mask1Application():
-    y = 0
-    for i in range (11):
-        Mask1ApplicationX(Y=y, X=0)
+def Mask1ApplicationX(Y = 0, X = 0):    #Masque
+    for i in range(11):
+        while X < FP.V1:
+            if FP.A[Y,X] == 1:
+                FP.A[Y,X] = 0
+                X = X +1
+            else:
+                FP.A[Y, X] = 1
+                X = X + 1
         y = y + 2
+
 
