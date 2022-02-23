bf386e028f56c817655d663f5c53038dbbfb3d77
Cornbald <adrien.lacroix@etu.univ-amu.fr>
2022-02-23 14:31:18 +0100

    Mask 000, Mask 001 and Mask 010

--- QRCode1/QRMask.py
+++ QRCode1/QRMask.py
@@ -1,5 +1,28 @@
 import QRFixedPatterns as FP
 
+def Mask000Application(Y = 0, X = 0):    #Masque 000
+    for i in range(11):
+        while X < FP.V1:
+            if FP.A[Y,X] == 1:
+                FP.A[Y,X] = 0
+                X = X +1
+                if FP.A[Y, X] == 1:
+                    FP.A[Y, X] = 1
+                    X = X + 1
+                else :
+                    FP.A[Y, X] = 0
+                    X = X + 1
+            else:
+                FP.A[Y, X] = 1
+                X = X + 1
+                if FP.A[Y, X] == 1:
+                    FP.A[Y, X] = 1
+                    X = X + 1
+                else:
+                    FP.A[Y, X] = 0
+                    X = X + 1
+        Y = Y + 1
+
 def Mask001Application(Y = 0, X = 0):    #Masque 001
     for i in range(11):
         while X < FP.V1:
@@ -9,7 +32,7 @@ def Mask001Application(Y = 0, X = 0):    #Masque 001
             else:
                 FP.A[Y, X] = 1
                 X = X + 1
-        Y = Y + 1
+        Y = Y + 2
 
 def Mask010Application(Y = 0, X = 0):    #Masque 010
     for i in range(11):
@@ -20,6 +43,6 @@ def Mask010Application(Y = 0, X = 0):    #Masque 010
             else:
                 FP.A[Y, X] = 1
                 Y = Y + 1
-        X = X + 1
+        X = X + 3
 
 
