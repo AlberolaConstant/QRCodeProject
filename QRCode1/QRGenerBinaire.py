31dabb2bcd0926d363d27af4288c0ebd25aaeffa
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2022-03-02 14:55:10 +0100

    img to binary (si ça marche pas faut juste changer le chemin d'acces de l'image)

--- QRCode1/QRGenerBinaire.py
+++ QRCode1/QRGenerBinaire.py
@@ -1,9 +1,16 @@
 from numpy import integer
+from PIL import Image
+import numpy
 
 Information = 'projet qr code ptut qui me soule grave quelle projet/cool(QRCODE)sdfd'
 i = 0
+k = 0
 caraNum = []
 liste = []
+imgbin = []
+
+img = Image.open("img/smile.jpg")
+np_img = numpy.array(img)
 
 print (Information)
 while i < len(Information):
@@ -11,5 +18,24 @@ while i < len(Information):
     liste.append(caraNum)
     i = i+1
 a = 0
+
+y=0
+
+
+def decimalToBinary(n):
+    return bin(n).replace("0b", "")
+
+for y in np_img:
+    for k in y:
+        for p in k:
+           conv = decimalToBinary(p)
+           if conv == "0":
+               conv = conv + "0000000"
+           elif conv == "1":
+               conv = "0000000" + conv
+        imgbin.append(conv)
+
+
 print (liste)
+print ("l'image:",imgbin)
 
