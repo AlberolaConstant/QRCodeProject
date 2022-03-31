6aa1ae122370c958ea2c1571b525c4970ae4221b
AlberolaConstant <constantalberola@gmail.com>
2022-03-31 15:13:29 +0200

    petit carre grand qr code

--- QRCode1/QRGenerBinaire.py
+++ QRCode1/QRGenerBinaire.py
@@ -2,7 +2,7 @@ from numpy import integer
 from PIL import Image
 import numpy
 
-Information = 'projet qr code ptut cool 2 fou'
+Information = 'projet qr code'
 i = 0
 k = 0
 caraNum = []
--- QRCode1/main.py
+++ QRCode1/main.py
@@ -6,13 +6,19 @@ import QRFormatInfo as FI
 import QRMessage as MS
 import QRMask as MA
 import Constante as C
+import ReedSolomon as RS
+import QRGenerBinaire as GB
 
 
 FP.calibragePattern()
-FI.InfoH()
+# FI.InfoH()
 # MS.messagePose(M=0, Y=C.VersionQR - 1, X=C.VersionQR - 1, D=7)
-# MA.Mask0ApplicationPro()
-MA.testmask()
+x = RS.ReedSolomon()
+x = x.RSEncode(GB.Information,11)
+x = RS.DecimalToBinary(x)
+print(x)
+
+# MA.testmask()
 
 # dessinez le QR code
 # print(FP.A)
