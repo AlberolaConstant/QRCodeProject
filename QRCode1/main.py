1b2aa949b15ee231907eecfe86525ffb601c362d
AlberolaConstant <constantalberola@gmail.com>
2022-03-31 18:51:16 +0200

    petit carre grand qr code marche super bien

--- QRCode1/main.py
+++ QRCode1/main.py
@@ -13,10 +13,10 @@ import QRGenerBinaire as GB
 FP.calibragePattern()
 # FI.InfoH()
 # MS.messagePose(M=0, Y=C.VersionQR - 1, X=C.VersionQR - 1, D=7)
-x = RS.ReedSolomon()
-x = x.RSEncode(GB.Information,11)
-x = RS.DecimalToBinary(x)
-print(x)
+# x = RS.ReedSolomon()
+# x = x.RSEncode(GB.Information,11)
+# x = RS.DecimalToBinary(x)
+# print(x)
 
 # MA.testmask()
 
